490b5da3b1c7e3ae5deed4cbb2d4ef4b18070fc1
Liewe Gutter <lgutter@student.codam.nl>
2020-01-13 21:15:59 +0100

    support in setenv for an emptied first element
    since unsetenv can in some cases empty the fields of the first element,
    setenv can nog use those emptied fields and fill them again.

--- srcs/ft_setenv.c
+++ srcs/ft_setenv.c
@@ -6,11 +6,22 @@
 /*   By: lgutter <lgutter@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/01/13 16:03:54 by lgutter        #+#    #+#                */
-/*   Updated: 2020/01/13 17:23:07 by lgutter       ########   odam.nl         */
+/*   Updated: 2020/01/13 20:52:34 by lgutter       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static int	fill_emptied_element(t_env *env, const char *key, const char *value)
+{
+	free(env->key);
+	free(env->value);
+	env->key = ft_strdup(key);
+	env->value = ft_strdup(value);
+	if (env->key == NULL || env->value == NULL)
+		return (ft_print_error(ERR_MALLOCFAIL));
+	return (0);
+}
 
 static int	ft_addenv(t_env *env, const char *key, const char *value)
 {
@@ -19,24 +30,18 @@ static int	ft_addenv(t_env *env, const char *key, const char *value)
 
 	current = env;
 	if (current == NULL)
-	{
 		return (ft_print_error(ERR_EMPTYENV));
-	}
+	if (ft_strcmp(env->key, "") == 0 && ft_strcmp(env->value, "") == 0)
+		return (fill_emptied_element(env, key, value));
 	new = (t_env *)ft_memalloc(sizeof(t_env) * 1);
 	if (new == NULL)
-	{
 		return (ft_print_error(ERR_MALLOCFAIL));
-	}
 	new->key = ft_strdup(key);
 	new->value = ft_strdup(value);
 	if (new->key == NULL || new->value == NULL)
-	{
 		return (ft_print_error(ERR_MALLOCFAIL));
-	}
 	while (current->next != NULL)
-	{
 		current = current->next;
-	}
 	current->next = new;
 	return (0);
 }
@@ -56,9 +61,7 @@ int			ft_setenv(t_env *env, const char *key,
 				free(current->value);
 				current->value = ft_strdup(value);
 				if (current->value == NULL)
-				{
 					return (ft_print_error(ERR_MALLOCFAIL));
-				}
 				return (0);
 			}
 			return (ft_print_error(ERR_ENVNOWRITE));
