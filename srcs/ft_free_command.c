c04a39e57dd476496dd53c2dbb94524dfb5e19ac
Liewe Gutter <lgutter@student.codam.nl>
2020-01-27 11:32:55 +0100

    expanded ft_free_command
    ft_free_command now properly frees everything

--- srcs/ft_free_command.c
+++ srcs/ft_free_command.c
@@ -6,7 +6,7 @@
 /*   By: lgutter <lgutter@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/01/22 14:10:47 by lgutter        #+#    #+#                */
-/*   Updated: 2020/01/22 14:19:36 by lgutter       ########   odam.nl         */
+/*   Updated: 2020/01/22 15:27:40 by lgutter       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,13 @@ int			ft_free_command(t_command *command)
 
 	index = 0;
 	free(command->input);
+	command->input = NULL;
 	ft_free_str_array(command->argv);
+	free(command->argv);
+	command->argv = NULL;
 	ft_free_str_array(command->envp);
+	free(command->envp);
+	command->envp = NULL;
+	command->argc = 0;
 	return (0);
 }
