daa69220f9e7398b61d769797368576d6ced584e
Liewe Gutter <lgutter@student.codam.nl>
2020-01-28 16:58:57 +0100

    updated handle_command plus tests
    handle_command now actually runs an executable if the correct path has been given.
    finding the executable in path still to come.

--- tests/srcs/ft_handle_command_tests.c
+++ tests/srcs/ft_handle_command_tests.c
@@ -6,7 +6,7 @@
 /*   By: lgutter <lgutter@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/01/27 10:58:45 by lgutter        #+#    #+#                */
-/*   Updated: 2020/01/27 11:31:53 by lgutter       ########   odam.nl         */
+/*   Updated: 2020/01/28 16:56:32 by lgutter       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,11 @@ static void redirect_std_out(void)
 	cr_redirect_stdout();
 }
 
+static void redirect_std_err(void)
+{
+	cr_redirect_stderr();
+}
+
 Test(unit_ft_split_command, basic_mandatory_handle_simple_command, .init = redirect_std_out)
 {
 	t_command	command;
@@ -30,11 +35,11 @@ Test(unit_ft_split_command, basic_mandatory_handle_simple_command, .init = redir
 	env->value = strdup("TESTENVVALUE");
 	env->next = NULL;
 
-	command.input = strdup("test arg1 arg2 arg3");
+	command.input = strdup("/bin/echo arg1 arg2 arg3");
 	ret = ft_handle_command(env, command);
 	fflush(stdout);
 	cr_assert_eq(ret, 0);
-	cr_assert_stdout_eq_str("command: test\nargc: 4\nargv[0]: test\nargv[1]: arg1\nargv[2]: arg2\nargv[3]: arg3\n");
+	cr_assert_stdout_eq_str("arg1 arg2 arg3\n");
 }
 
 Test(unit_ft_split_command, basic_mandatory_handle_dollar_expansion_in_arg, .init = redirect_std_out)
@@ -47,11 +52,11 @@ Test(unit_ft_split_command, basic_mandatory_handle_dollar_expansion_in_arg, .ini
 	env->value = strdup("TESTENVVALUE");
 	env->next = NULL;
 
-	command.input = strdup("test $TESTENVKEY arg2 arg3");
+	command.input = strdup("/bin/echo $TESTENVKEY arg2");
 	ret = ft_handle_command(env, command);
 	fflush(stdout);
 	cr_assert_eq(ret, 0);
-	cr_assert_stdout_eq_str("command: test\nargc: 4\nargv[0]: test\nargv[1]: TESTENVVALUE\nargv[2]: arg2\nargv[3]: arg3\n");
+	cr_assert_stdout_eq_str("TESTENVVALUE arg2\n");
 }
 
 Test(unit_ft_split_command, basic_mandatory_handle_home_expansion_in_arg, .init = redirect_std_out)
@@ -64,10 +69,29 @@ Test(unit_ft_split_command, basic_mandatory_handle_home_expansion_in_arg, .init 
 	env->value = strdup("/Users/lgutter");
 	env->next = NULL;
 
-	command.input = strdup("test ~ arg2 arg3");
+	command.input = strdup("/bin/echo arg ~");
 	ret = ft_handle_command(env, command);
 	fflush(stdout);
 	cr_assert_eq(ret, 0);
-	cr_assert_stdout_eq_str("command: test\nargc: 4\nargv[0]: test\nargv[1]: /Users/lgutter\nargv[2]: arg2\nargv[3]: arg3\n");
+	cr_assert_stdout_eq_str("arg /Users/lgutter\n");
 }
 
+Test(unit_ft_split_command, basic_mandatory_error_command_not_found, .init = redirect_std_err)
+{
+	t_command	command;
+	int			ret;
+	t_env		*env = (t_env *)malloc(sizeof(t_env) * 1);
+
+	env->key = strdup("HOME");
+	env->value = strdup("/Users/lgutter");
+	env->next = NULL;
+
+	printf("create input\n");
+	command.input = strdup("test arg1 arg2");
+	printf("RUN\n");
+	ret = ft_handle_command(env, command);
+	fflush(stderr);
+	cr_assert_stderr_eq_str("-ish: test: command not found\n");
+	printf("ASSERTS\n");
+	cr_assert_eq(ret, ERR_CMD_NOT_FOUND);
+}
