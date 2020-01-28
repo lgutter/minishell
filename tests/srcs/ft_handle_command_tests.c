cd2b6b4c3bdf56515c99c9d0e074605c2d17f6fe
Liewe Gutter <lgutter@student.codam.nl>
2020-01-28 17:25:28 +0100

    modified handle_command_tests to use print
    since travis does not have /bin/echo, modified a test to use /usr/bin/printf

--- tests/srcs/ft_handle_command_tests.c
+++ tests/srcs/ft_handle_command_tests.c
@@ -6,7 +6,7 @@
 /*   By: lgutter <lgutter@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/01/27 10:58:45 by lgutter        #+#    #+#                */
-/*   Updated: 2020/01/28 16:56:32 by lgutter       ########   odam.nl         */
+/*   Updated: 2020/01/28 17:25:11 by lgutter       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,12 +52,13 @@ Test(unit_ft_split_command, basic_mandatory_handle_dollar_expansion_in_arg, .ini
 	env->value = strdup("TESTENVVALUE");
 	env->next = NULL;
 
-	command.input = strdup("/bin/echo $TESTENVKEY arg2");
+	command.input = strdup("/usr/bin/printf %s\n $TESTENVKEY arg2");
 	ret = ft_handle_command(env, command);
 	fflush(stdout);
 	cr_assert_eq(ret, 0);
-	cr_assert_stdout_eq_str("TESTENVVALUE arg2\n");
+	cr_assert_stdout_eq_str("TESTENVVALUE\narg2\n");
 }
+
 
 Test(unit_ft_split_command, basic_mandatory_handle_home_expansion_in_arg, .init = redirect_std_out)
 {
@@ -86,12 +87,9 @@ Test(unit_ft_split_command, basic_mandatory_error_command_not_found, .init = red
 	env->value = strdup("/Users/lgutter");
 	env->next = NULL;
 
-	printf("create input\n");
 	command.input = strdup("test arg1 arg2");
-	printf("RUN\n");
 	ret = ft_handle_command(env, command);
 	fflush(stderr);
 	cr_assert_stderr_eq_str("-ish: test: command not found\n");
-	printf("ASSERTS\n");
 	cr_assert_eq(ret, ERR_CMD_NOT_FOUND);
 }
